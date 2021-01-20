fc84df05142cd8d35e28767f287bff1233feac12
david <davidsolo555@hotmail.com>
2021-01-19 17:57:43 -0800

    added server endpoints

--- server.js
+++ server.js
@@ -15,6 +15,35 @@ import { Game, move, status, moves, aiMove } from "./chess-engine/js-chess-engin
 import { NEW_GAME_BOARD_CONFIG } from "./chess-engine/const/board.mjs";
 console.log(moves(NEW_GAME_BOARD_CONFIG));
 
+//auth0 set up
+const { auth } = require("express-openid-connect");
+const { requiresAuth } = require("express-openid-connect");
+
+const config = {
+    authRequired: false,
+    auth0Logout: true,
+    secret: "a long, randomly-generated string stored in env",
+    baseURL: "http://localhost:3000",
+    clientID: "55ZMdiGL5bT0Y3M7OA3r5uQAQgcmJBR7",
+    issuerBaseURL: "https://dev-490elg7s.us.auth0.com",
+};
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
+
+app.get("/", (req, res, next) => {
+    res.send("Welcome to muggle chess");
+});
+
+// req.isAuthenticated is provided from the auth router
+app.get("/login", (req, res) => {
+    res.send(req.oidc.isAuthenticated() ? "Logged in" : "Logged out");
+});
+
+app.get("/profile", requiresAuth(), (req, res) => {
+    res.send(JSON.stringify(req.oidc.user));
+});
+
 app.get("/api/newgame", (req, res, next) => {
     res.send(moves(NEW_GAME_BOARD_CONFIG));
 });
