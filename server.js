78dd59037a015e62d6bb522359fe0a550d24cfb3
david <davidsolo555@hotmail.com>
2021-01-22 18:10:25 -0800

    added validation to client side

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const http = require("http");
 const app = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const auth0Config = require("./config/config");
 
 //setup middleware
 
@@ -33,17 +34,8 @@ import { NEW_GAME_BOARD_CONFIG } from "./chess-engine/const/board.mjs";
 const { auth } = require("express-openid-connect");
 const { requiresAuth } = require("express-openid-connect");
 
-const config = {
-    authRequired: false,
-    auth0Logout: true,
-    secret: "a long, randomly-generated string stored in env",
-    baseURL: "http://localhost:3000",
-    clientID: "55ZMdiGL5bT0Y3M7OA3r5uQAQgcmJBR7",
-    issuerBaseURL: "https://dev-490elg7s.us.auth0.com",
-};
-
 // auth router attaches /login, /logout, and /callback routes to the baseURL
-app.use(auth(config));
+app.use(auth(auth0Config));
 
 app.get("/", (req, res, next) => {
     res.send("Welcome to muggle chess");
@@ -65,29 +57,7 @@ app.get("/api/newgame", (req, res, next) => {
     res.send(getFen(NEW_GAME_BOARD_CONFIG));
 });
 
-//TODO add saved games from db
-
-//TODO add validation logic
-app.get("/api/validate", (req, res, next) => {
-    res.send({ "result": true });
-});
-
 //Returns the updated board state after a given move is calculated
 app.post("/api/move", (req, res, next) => {
-    try {
-        let updatedFen = move(req.body.fen, req.body.moveFrom.toUpperCase(), req.body.moveTo.toUpperCase());
-        res.send(getFen(updatedFen));
-    } catch (err) {
-        res.send(req.body.fen);
-    }
-});
-
-//return new board state after AI makes a move
-app.post("/api/aimove", (req, res, next) => {
-    let boardState = req.body.boardState;
-    let ai = aiMove(boardState, 2);
-    let from = Object.keys(ai)[0];
-    let to = ai[from];
-    let updatedBoard = move(boardState, from, to);
-    res.send(getFen(updatedBoard));
+    console.log(`New board state: ${req.body.fen}`);
 });
