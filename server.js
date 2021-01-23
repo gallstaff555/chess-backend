079de4be62ff279d0a14e3923be36f1f3b091fac
david <davidsolo555@hotmail.com>
2021-01-22 20:44:33 -0800

    original cors settings

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const bodyParser = require("body-parser");
 //CORS set up
 const cors = require("cors");
 app.use(cors());
+app.options("*", cors());
 //app.options("*", cors());
 // app.use(
 //     cors({
@@ -20,13 +21,13 @@ app.use(cors());
 //app.options("*", cors());
 //
 
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://gallstaff555.github.io/muggle-chess-client/");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
-    res.header("Access-Control-Allow-Credentials", true);
-    next();
-});
+// app.use(function (req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "https://gallstaff555.github.io/muggle-chess-client/");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
+//     res.header("Access-Control-Allow-Credentials", true);
+//     next();
+// });
 
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
